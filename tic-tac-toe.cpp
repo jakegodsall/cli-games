f31459aba34914420b9888a0c75fb8bcc2ee374e
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-21 07:25:37 +0100

    added input validation

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -8,6 +8,7 @@ const int COLS = 3;
 void runGame();
 void initialiseGameBoard(std::array<std::array<char, COLS>, ROWS>& gameBoard);
 void printCurrentGameBoard(std::array<std::array<char, COLS>, ROWS>& gameBoard);
+bool validateUserInput(int col, int row);
 void getUserInput(bool xTurn, std::array<std::array<char, COLS>, ROWS> gameBoard);
 bool cellAlreadyOccupied(int row, int col, std::array<std::array<char, COLS>, ROWS> gameBoard);
 std::string getWinner(std::array<std::array<char, COLS>, ROWS> gameBoard);
@@ -28,8 +29,8 @@ void runGame() {
     std::array<std::array<char, COLS>, ROWS> gameBoard;
     // initialise the game board
     initialiseGameBoard(gameBoard);
-
     printCurrentGameBoard(gameBoard);
+    getUserInput(true, gameBoard);
 
 }
 
@@ -64,14 +65,41 @@ void printCurrentGameBoard(std::array<std::array<char, COLS>, ROWS>& gameBoard) 
     }
 }
 
+bool validateUserInput(int col, int row) {
+    if ((col >= 0 && col < COLS) && (row >= 0 && row < ROWS)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 void getUserInput(bool xTurn, std::array<std::array<char, COLS>, ROWS> gameBoard) {
     char currentPlayer = xTurn ? 'X' : 'O';
-    std::cout << "It is " << currentPlayer << "'s turn" << std::endl;
 
+    std::cout << "It is " << currentPlayer << "'s turn" << std::endl;
     std::cout << "Please enter the row THEN the column, each from 0, 1, or 2, separated by a space" << std::endl;
 
-    int rowValue, colVal;
+    int colValue;
+    int rowValue;
+    std::cin >> colValue >> rowValue;
 
-    std::cin >> rowValue;
-    std::cin >> colVal;
+    // validate inputs
+    while (true) {
+        if (std::cin.fail()) {
+            std::cout << "You did not enter an integer. " << std::endl;
+
+            std::cin.clear(); // clear the error state of the cin object
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            std::cout << "Try again: " << std::endl;
+            std::cin >> colValue >> rowValue;
+        } else {
+            std::cout << "Entered values:" << std::endl;
+            std::cout << "Column: " << colValue << std::endl;
+            std::cout << "Row: " << rowValue << std::endl << std::endl;
+
+            break;
+        }
+    }
+
 }
